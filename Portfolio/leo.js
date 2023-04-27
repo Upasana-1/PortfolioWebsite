e59b742bff7e875f8b035e7b4ebebdba9c4e540b
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2023-04-27 16:41:04 +0545

    fixed bug

--- Portfolio/leo.js
+++ Portfolio/leo.js
@@ -1,33 +1,30 @@
-Let menuIcon= document.querySelector('#menu-icon');
-Let navbar= document.querySelector('#navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle(' bx-x');
-    menuIcon.classList.toggle(' active');
+    // menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
-
-
-
-
 
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header navbar a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
-        Let top = window.scrollY;
-        Let offset = sec.offsetTop - 150;
-        Let height = sec.offsetHeight;
-        Let id = sec.getAttribute('id'));
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navlinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
-    });
+    })
+};
 
-    let header = document.querySelector
+let header = document.querySelector
 
 
